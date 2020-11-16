236ed1756af88144a024ca15b10548181926974a
wonjwi <wonji616@gmail.com>
2020-11-16 18:07:56 +0900

    Change classtudy_SQL file : txt -> sql

--- DB관련자료/classtudy_SQL.sql
+++ DB관련자료/classtudy_SQL.sql
@@ -3,20 +3,20 @@
 ---------------------------------------------------
 -- 관리자 테이블 생성
 CREATE TABLE admin (
-	adminId 	VARCHAR(16) PRIMARY KEY,
-	passwd 		VARCHAR(16) NOT NULL
+	adminId 	VARCHAR(16) 	PRIMARY KEY,
+	passwd 		VARCHAR(16) 	NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- 학원 테이블 생성
 CREATE TABLE academy (
-	academyId 	INT 		PRIMARY KEY AUTO_INCREMENT,
-	academyName VARCHAR(30) NOT NULL
+	academyId 	INT PRIMARY KEY AUTO_INCREMENT,
+	academyName 	VARCHAR(30) 	NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- 강의 테이블 생성
 CREATE TABLE lecture (
-	lectureId 	INT 		PRIMARY KEY AUTO_INCREMENT,
-	lectureName VARCHAR(60) NOT NULL,
+	lectureId 	INT PRIMARY KEY AUTO_INCREMENT,
+	lectureName 	VARCHAR(60) 	NOT NULL,
 	academyId 	INT 		NOT NULL,
 	FOREIGN KEY (academyId) REFERENCES academy(academyId)	
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -28,29 +28,29 @@ CREATE TABLE lecture (
 -- leader 필요하면 나중에 leader table 생성해서 memberId, groupId 연결
 CREATE TABLE grouplist (
 	groupId 	INT 		PRIMARY KEY AUTO_INCREMENT,
-	groupName 	VARCHAR(30) NOT NULL	
+	groupName 	VARCHAR(30) 	NOT NULL	
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- 멤버 테이블 생성
 CREATE TABLE member (
 	memberId 	VARCHAR(16) 	PRIMARY KEY,
 	passwd 		VARCHAR(16) 	NOT NULL,
-    name 		VARCHAR(16) 	NOT NULL,
-    dateOfBirth VARCHAR(10) 	NOT NULL,
-    gender 		VARCHAR(2) 		NOT NULL,
-    tel 		VARCHAR(16) 	NOT NULL,
-    zipcode 	VARCHAR(8) 		NOT NULL,
-    address 	VARCHAR(60) 	NOT NULL,
-    addressDetail VARCHAR(60) 	NOT NULL,
-    email 		VARCHAR(40) 	NOT NULL,
-    regDate 	TIMESTAMP 		NOT NULL,
-	lectureId 	INT 			NOT NULL,
-	grade 		INT 			NOT NULL DEFAULT 0,
-	point 		INT 			NOT NULL DEFAULT 0,
-	reward 		INT 			NOT NULL DEFAULT 0,
-	group1 		INT 			NOT NULL DEFAULT 1,
-	group2 		INT 			NOT NULL DEFAULT 1,
-	group3 		INT 			NOT NULL DEFAULT 1,	
+	name 		VARCHAR(16) 	NOT NULL,
+	dateOfBirth 	VARCHAR(10) 	NOT NULL,
+	gender 		VARCHAR(2) 	NOT NULL,
+	tel 		VARCHAR(16) 	NOT NULL,
+	zipcode 	VARCHAR(8) 	NOT NULL,
+	address 	VARCHAR(60) 	NOT NULL,
+	addressDetail 	VARCHAR(60) 	NOT NULL,
+	email 		VARCHAR(40) 	NOT NULL,
+	regDate 	TIMESTAMP 	NOT NULL,
+	lectureId 	INT 		NOT NULL,
+	grade 		INT 		NOT NULL DEFAULT 0,
+	point 		INT 		NOT NULL DEFAULT 0,
+	reward 		INT 		NOT NULL DEFAULT 0,
+	group1 		INT 		NOT NULL DEFAULT 1,
+	group2 		INT 		NOT NULL DEFAULT 1,
+	group3 		INT 		NOT NULL DEFAULT 1,	
 	FOREIGN KEY (lectureId) REFERENCES lecture(lectureId),
 	FOREIGN KEY (group1) REFERENCES grouplist(groupId),
 	FOREIGN KEY (group2) REFERENCES grouplist(groupId),
@@ -59,31 +59,31 @@ CREATE TABLE member (
 
 -- 알림 테이블
 CREATE TABLE noti (
-	notiId 		INT 			PRIMARY KEY AUTO_INCREMENT,
+	notiId 		INT 		PRIMARY KEY AUTO_INCREMENT,
 	receiver 	VARCHAR(16) 	NOT NULL,
 	content 	VARCHAR(100) 	NOT NULL,
-	checked 	BOOLEAN 		NOT NULL DEFAULT false,
-	sendDate 	TIMESTAMP 		NOT NULL,
+	checked 	BOOLEAN 	NOT NULL DEFAULT false,
+	sendDate 	TIMESTAMP 	NOT NULL,
 	FOREIGN KEY (receiver) REFERENCES member(memberId)	
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- 포인트 테이블
 CREATE TABLE point (
-	pointId 	INT 			PRIMARY KEY AUTO_INCREMENT,
+	pointId 	INT 		PRIMARY KEY AUTO_INCREMENT,
 	member 		VARCHAR(16) 	NOT NULL,
 	content 	VARCHAR(100) 	NOT NULL,
-	changeVal 	INT 			NOT NULL,
-	accrDate 	TIMESTAMP 		NOT NULL,
+	changeVal 	INT 		NOT NULL,
+	accrDate 	TIMESTAMP 	NOT NULL,
 	FOREIGN KEY (member) REFERENCES member(memberId)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- 적립금 테이블
 CREATE TABLE reward (
-	rewardId 	INT 			PRIMARY KEY AUTO_INCREMENT,
+	rewardId 	INT 		PRIMARY KEY AUTO_INCREMENT,
 	member 		VARCHAR(16) 	NOT NULL,
 	content 	VARCHAR(100) 	NOT NULL,
-	changeVal 	INT 			NOT NULL,
-	accrDate 	TIMESTAMP 		NOT NULL,
+	changeVal 	INT 		NOT NULL,
+	accrDate 	TIMESTAMP 	NOT NULL,
 	FOREIGN KEY (member) REFERENCES member(memberId)	
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -93,8 +93,8 @@ CREATE TABLE reward (
 -- 클래스게시판 테이블 생성
 CREATE TABLE classboard (
 	boardId 	INT 		PRIMARY KEY AUTO_INCREMENT,
-	writer 		VARCHAR(16) NOT NULL,
-	title 		VARCHAR(50) NOT NULL,
+	writer 		VARCHAR(16) 	NOT NULL,
+	title 		VARCHAR(50) 	NOT NULL,
 	content 	TEXT 		NOT NULL,
 	category 	SET('TIL', '질문', '클래스') NOT NULL,
 	views 		INT 		NOT NULL DEFAULT 0,
@@ -108,28 +108,36 @@ CREATE TABLE classboard (
 -- 자유게시판 테이블 생성
 CREATE TABLE freeboard (
 	boardId 	INT 		PRIMARY KEY AUTO_INCREMENT,
-	writer 		VARCHAR(16) NOT NULL,
-	title 		VARCHAR(50) NOT NULL,
+	writer 		VARCHAR(16) 	NOT NULL,
+	title 		VARCHAR(50) 	NOT NULL,
 	content 	TEXT 		NOT NULL,
-	category 	SET('공지사항', '질문', '정보', '이야기') NOT NULL,
+	category 	SET('공지사항', '질문', '정보', '이야기', '그룹홍보') NOT NULL,
 	views 		INT 		NOT NULL DEFAULT 0,
 	likes 		INT 		NOT NULL DEFAULT 0,
 	writeDate 	TIMESTAMP 	NOT NULL,
-	tags 		SET('Java', 'Python', 'JavaScript', 'Back-End', '웹 개발', '앱 개발', 'HTML/CSS', 'Front-End', '데이터베이스', '게임 개발', 'Android', 'Spring', 'Node.js', '알고리즘', 'React', '머신러닝', 'SQL', '인공지능', '정보보안', '블록체인', 'DevOps', '클라우드', 'Linux', 'iOS', 'Swift'),
+	tags 		SET('Java', 'Python', 'JavaScript', 'Back-End', '웹 개발', '앱 개발', 'HTML/CSS', 'Front-End', '데이터베이스', 
+			    '게임 개발', 'Android', 'Spring', 'Node.js', '알고리즘', 'React', '머신러닝', 'SQL', '인공지능', '정보보안', 
+			    '블록체인', 'DevOps', '클라우드', 'Linux', 'iOS', 'Swift', 'Spring Boot', 'Bootstrap', 'jQuery', 'Git', 
+			    'Django', 'UI/UX', '3D 모델링', 'Flask', 'After Effects', 'Premiere Pro', 'Photoshop', 'Illustrator', 
+			    'R', '편집 디자인', 'Unity', 'JSP', 'JDBC', 'JSTL', '해킹', 'InDesign'),
 	FOREIGN KEY (writer) REFERENCES member(memberId)	
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- 그룹게시판 테이블 생성
 CREATE TABLE groupboard (
 	boardId 	INT 		PRIMARY KEY AUTO_INCREMENT,
-	writer 		VARCHAR(16) NOT NULL,
-	title 		VARCHAR(50) NOT NULL,
+	writer 		VARCHAR(16) 	NOT NULL,
+	title 		VARCHAR(50) 	NOT NULL,
 	content 	TEXT 		NOT NULL,
 	views 		INT 		NOT NULL DEFAULT 0,
 	likes 		INT 		NOT NULL DEFAULT 0,
 	writeDate 	TIMESTAMP 	NOT NULL,
 	groupId 	INT 		NOT NULL,
-	tags 		SET('Java', 'Python', 'JavaScript', 'Back-End', '웹 개발', '앱 개발', 'HTML/CSS', 'Front-End', '데이터베이스', '게임 개발', 'Android', 'Spring', 'Node.js', '알고리즘', 'React', '머신러닝', 'SQL', '인공지능', '정보보안', '블록체인', 'DevOps', '클라우드', 'Linux', 'iOS', 'Swift'),
+	tags 		SET('Java', 'Python', 'JavaScript', 'Back-End', '웹 개발', '앱 개발', 'HTML/CSS', 'Front-End', '데이터베이스', 
+			    '게임 개발', 'Android', 'Spring', 'Node.js', '알고리즘', 'React', '머신러닝', 'SQL', '인공지능', '정보보안', 
+			    '블록체인', 'DevOps', '클라우드', 'Linux', 'iOS', 'Swift' 'Spring Boot', 'Bootstrap', 'jQuery', 'Git', 
+			    'Django', 'UI/UX', '3D 모델링', 'Flask', 'After Effects', 'Premiere Pro', 'Photoshop', 'Illustrator', 
+			    'R', '편집 디자인', 'Unity', 'JSP', 'JDBC', 'JSTL', '해킹', 'InDesign'),
 	FOREIGN KEY (writer) REFERENCES member(memberId),
 	FOREIGN KEY (groupId) REFERENCES grouplist(groupId)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -139,34 +147,34 @@ CREATE TABLE groupboard (
 ---------------------------------------------------
 -- 클래스게시판 댓글 테이블 생성
 CREATE TABLE cbcomment (
-	commentId 	INT 			PRIMARY KEY AUTO_INCREMENT,
+	commentId 	INT 		PRIMARY KEY AUTO_INCREMENT,
 	writer 		VARCHAR(16) 	NOT NULL,
-	content 	TEXT 			NOT NULL,
-	writeDate 	TIMESTAMP 		NOT NULL,
-	boardId 	INT 			NOT NULL,
+	content 	TEXT 		NOT NULL,
+	writeDate 	TIMESTAMP 	NOT NULL,
+	boardId 	INT 		NOT NULL,
 	FOREIGN KEY (writer) REFERENCES member(memberId),
 	FOREIGN KEY (boardId) REFERENCES classboard(boardId)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- 자유게시판 댓글 테이블 생성
 CREATE TABLE fbcomment (
-	commentId 	INT 			PRIMARY KEY AUTO_INCREMENT,
+	commentId 	INT 		PRIMARY KEY AUTO_INCREMENT,
 	writer 		VARCHAR(16) 	NOT NULL,
-	content 	TEXT 			NOT NULL,
-	likes 		INT 			NOT NULL DEFAULT 0,
-	writeDate 	TIMESTAMP 		NOT NULL,
-	boardId 	INT 			NOT NULL,
+	content 	TEXT 		NOT NULL,
+	likes 		INT 		NOT NULL DEFAULT 0,
+	writeDate 	TIMESTAMP 	NOT NULL,
+	boardId 	INT 		NOT NULL,
 	FOREIGN KEY (writer) REFERENCES member(memberId),
 	FOREIGN KEY (boardId) REFERENCES freeboard(boardId)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- 그룹게시판 댓글 테이블 생성
 CREATE TABLE gbcomment (
-	commentId 		INT 			PRIMARY KEY AUTO_INCREMENT,
+	commentId 	INT 		PRIMARY KEY AUTO_INCREMENT,
 	writer 		VARCHAR(16) 	NOT NULL,
-	content 	TEXT 			NOT NULL,
-	writeDate 	TIMESTAMP 		NOT NULL,
-	boardId 	INT 			NOT NULL,
+	content 	TEXT 		NOT NULL,
+	writeDate 	TIMESTAMP 	NOT NULL,
+	boardId 	INT 		NOT NULL,
 	FOREIGN KEY (writer) REFERENCES member(memberId),
 	FOREIGN KEY (boardId) REFERENCES groupboard(boardId)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
